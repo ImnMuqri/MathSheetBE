119b1abb49b6fdd38f8012908a5699648b9ed34e
ImnMuqri <imnmuqri@gmail.com>
2024-06-09 21:27:45 +0800

    Config

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ try {
 // Routes
 app.use("/api", playerRoutes);
 
-const port = 9000;
+const port = process.env.PORT || 9000;
 app.listen(port, () => {
   console.log("Server started on port " + port);
 });
